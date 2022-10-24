6d4ef8a871862aa44a2effbf145fb60278da9c08
dmvrt <85402053+dmvrt@users.noreply.github.com>
2022-10-24 23:04:17 +0300

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -5,7 +5,7 @@ YELLOW="\033[33m"
 GREEN="\033[32m"
 MAGENTA="\033[35m"
 NORMAL="\033[0m"
-
+TWITTER="https://twitter.com/dmvrt5"
 echo ""
 echo -e "$YELLOW░░$NORMAL$MAGENTA┌┐$NORMAL$YELLOW░░░░░░░░$NORMAL$MAGENTA┌┐$NORMAL$YELLOW░░░░░░░░░░░░░░$NORMAL$MAGENTA┌┐$NORMAL$YELLOW░░░░$NORMAL"
 echo -e "$YELLOW░░$NORMAL$MAGENTA││$NORMAL$YELLOW░░░░░░░$NORMAL$MAGENTA┌┘└┐$NORMAL$YELLOW░░░░░░░░░░░░$NORMAL$MAGENTA┌┘└┐$NORMAL$YELLOW░░░$NORMAL"
@@ -15,7 +15,7 @@ echo -e "$MAGENTA│└┘│││├┐┌┤│$MAGENTA$YELLOW░$NORMAL$MAGE
 echo -e "$MAGENTA└──┴┴┴┘└┘└┘$MAGENTA$YELLOW░$NORMAL$MAGENTA└─┘└──┴──┴┘└┤┌─┘└─┴──┘$NORMAL"
 echo -e "$YELLOW░░░░░░░░░░░░░░░░░░░░░░░░$NORMAL$MAGENTA││$NORMAL$YELLOW░░░░░░░░$NORMAL"
 echo -e "$YELLOW░░░░░░░░░░░░░░░░░░░░░░░░$NORMAL$MAGENTA└┘$NORMAL$YELLOW░░░░░░░░$NORMAL"
-echo -e "$YELLOW░░░░$NORMAL\https://twitter.com/dmvrt5$NORMAL$YELLOW░░░░░░░░░$NORMAL"
+echo -e "$YELLOW░░░░$NORMAL░░░░░░░░$TWITTER░░░░░░░░$NORMAL$YELLOW░░░░░░$NORMAL"
 echo ""
 
 sleep 1
